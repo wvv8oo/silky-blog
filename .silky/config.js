aa88fe9288e4c3819b9cdc8aad70ff16abdb0f87
wvv8oo <wvv8oo@gmail.com>
2015-02-13 11:37:30 +0800

    初始化项目

---
+++ .silky/config.js
@@ -0,0 +1,57 @@
+module.exports = {
+    //配置文件的版本，和silky的版本无关
+    version: 0.2,
+    //80端口在*nix下需要sudo
+    port: 14422,
+    //代理配置相关，兼容json-proxy的代理配置
+    proxy: {
+        forward: {
+            //定义代理转发
+            //"/ajax": "/"
+        }
+    },
+    //路由
+    routers: [
+         {
+             //path: 原路径，to: 替换后的路径，next：是否继承执行下一个路由替换，static：是否为静态文件，静态文件直接返回
+             path: /^\/$/, to: 'index.html', next: true
+         }
+    ],
+    //插件的配置
+    plugins: {
+        "honey-preview": {
+            server: "108"
+        }
+    },
+    //build的配置
+    build: {
+        //构建的目标目录，命令行指定的优先
+        output: "./build",
+        //将要复制的文件目录，直接复制到目标
+        copy: [/^images(\-demo)?$/i],
+        //完全忽略处理的文件
+        ignore: [/^template\/module$/i, /^css\/module$/i, /^\..+/],
+        //重命名
+        rename: [
+            {
+                source: /source\.(js)$/i, target: '$1', next: false
+            },
+            {
+                source: /^template\/(.+)/i, target: '$1', next: false
+            }
+        ],
+        //是否压缩
+        compress: {
+            //将要忽略压缩的文件
+            ignore: [],
+            //压缩js，包括coffee
+            js: true,
+            //压缩css，包括less
+            css: true,
+            //压缩html
+            html: false,
+            //是否压缩internal的js
+            internal: true
+        }
+    }
+}
